0e0663d75388bcb89334deef489f5d0a6c20c899
Michelle Wong Yu Miin <mymw@uw.edu>
2020-08-19 11:59:12 -0700

    Added new changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let addUser = document.querySelector('');
+addUser.addEventListener('submit', function(newUser) {
+    newUser.preventDefault();
+    //Create new page
+    //Add user's name
+})
+
+let addBook = document.querySelector('#addBook');
+addBook.addEventListener('click', function(add) {
+    add.preventDefault();
+    //Move all info to respective places
+})
